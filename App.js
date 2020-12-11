dff17550e3ae70153c50251ca7b4b09bba8c283d
Mesaque Francisco <mesaquenf@gmail.com>
2020-12-11 09:16:06 -0300

    Adding Bottom Navigation

--- App.js
+++ App.js
@@ -1,6 +1,6 @@
 import React, { useEffect } from 'react'
 import { Button, View, Text, SafeAreaView, StatusBar } from 'react-native'
-import { createDrawerNavigator } from '@react-navigation/drawer'
+import { createBottomTabNavigator } from '@react-navigation/bottom-tabs'
 import { NavigationContainer } from '@react-navigation/native'
 import { Provider as PaperProvider } from 'react-native-paper'
 import { observer } from 'mobx-react-lite'
@@ -10,29 +10,36 @@ import Logger from './src/utils/Logger'
 import ProductList from './src/screens/product-list/ProductList'
 import LoginScreen from './src/screens/login/LoginScreen'
 
-const Drawer = createDrawerNavigator()
+const Tab = createBottomTabNavigator()
 
 export default function App () {
   return (
     <PaperProvider>
       <NavigationContainer>
         <StatusBar />
-        <Drawer.Navigator initialRouteName='ProductList'>
-          <Drawer.Screen
+        <Tab.Navigator initialRouteName='ProductList'>
+          <Tab.Screen
             name='ProductList'
             component={ProductList}
             options={{
               title: 'Produtos disponíveis'
             }}
           />
-          <Drawer.Screen
+          <Tab.Screen
+            name='Catalog'
+            component={ProductList}
+            options={{
+              title: 'Gerenciar Catálogo'
+            }}
+          />
+          <Tab.Screen
             name='Login'
             component={LoginScreen}
             options={{
-              title: 'Sair'
+              title: 'Conta'
             }}
           />
-        </Drawer.Navigator>
+        </Tab.Navigator>
       </NavigationContainer>
     </PaperProvider>
   )
