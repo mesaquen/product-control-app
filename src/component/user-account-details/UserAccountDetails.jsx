36b5081b5d34d0e081becaf671afbe488bee7c33
Mesaque Francisco <mesaquenf@gmail.com>
2020-12-10 23:58:30 -0300

    Creating UserAccounDetails component

---
+++ src/component/user-account-details/UserAccountDetails.jsx
@@ -0,0 +1,27 @@
+import React from 'react'
+import { observer } from 'mobx-react'
+import userStore from '../../mobx/UserStore'
+import IconMessage from '../icon-message/IconMessage'
+
+const UserAccountDetails = observer(() => {
+  const handleLogoff = () => userStore.setUserContext(null)
+
+  if (userStore.userContext) {
+    const {
+      user: { email, username }
+    } = userStore.userContext
+    debugger
+    return (
+      <IconMessage
+        title={username}
+        subtitle={email}
+        buttonLabel='Desconectar'
+        onPress={handleLogoff}
+      />
+    )
+  }
+
+  return null
+})
+
+export default UserAccountDetails
