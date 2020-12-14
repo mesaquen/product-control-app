444b166856963b5c134f74ac0c8705df41eee717
Mesaque Francisco <mesaquenf@gmail.com>
2020-12-14 10:25:33 -0300

    Adding intro slide

--- App.js
+++ App.js
@@ -1,19 +1,23 @@
-import React, { useEffect } from 'react'
-import { StatusBar } from 'react-native'
+import React, { useState, useEffect } from 'react'
+import { SafeAreaView, StatusBar } from 'react-native'
 import { createBottomTabNavigator } from '@react-navigation/bottom-tabs'
 import { createStackNavigator } from '@react-navigation/stack'
 import { NavigationContainer } from '@react-navigation/native'
 import {
   DefaultTheme,
   Colors,
-  Provider as PaperProvider
+  Provider as PaperProvider,
+  Text,
+  Button
 } from 'react-native-paper'
 import { MaterialCommunityIcons as MCI } from '@expo/vector-icons'
 import ProductList from './src/screens/product-list/ProductList'
 import LoginScreen from './src/screens/login/LoginScreen'
 import CatalogScreen from './src/screens/catalog/CatalogScreen'
 import ProductForm from './src/screens/product-form/ProductForm'
+import AppIntroSlider from 'react-native-app-intro-slider'
 import { observer } from 'mobx-react'
+import IconMessage from './src/component/icon-message/IconMessage'
 
 const Tab = createBottomTabNavigator()
 const Stack = createStackNavigator()
@@ -55,52 +59,108 @@ const CatalogStackScreen = () => {
         }
       }}
     >
-      <Stack.Screen name="CatalogList" component={CatalogScreen} options={{title: 'Catálogo'}}/>
+      <Stack.Screen
+        name='CatalogList'
+        component={CatalogScreen}
+        options={{ title: 'Catálogo' }}
+      />
       <Stack.Screen name='ProductForm' component={ProductForm} />
     </Stack.Navigator>
   )
 }
 
+const SLIDES = [
+  {
+    key: 'k1',
+    title: 'Product control app',
+    text: 'Bem vindo ao app de controle de produto'
+  },
+  {
+    key: 'k2',
+    icon: icons.ProductList.default,
+    title: 'Produtos disponíveis',
+    text: 'Use esse menu para vizualizar os produtos ativos'
+  },
+  {
+    key: 'k3',
+    icon: icons.Catalog.default,
+    title: 'Gerenciar catálogo',
+    text: 'Aqui você pode adicionar, editar ou remover produtos'
+  },
+  {
+    key: 'k4',
+    icon: icons.Login.default,
+    title: 'Conta',
+    text: 'Use esse menu para acessar sua conta'
+  }
+]
+
 export default function App () {
+  const [showSlide, setShowSlide] = useState(true)
+  const hideSlide = () => setShowSlide(false)
+
+  const renderSliderItem = ({item}) => (
+    <SafeAreaView style={{flex: 1}}>
+      <IconMessage icon={item.icon} title={item.title} subtitle={item.text}/>
+    </SafeAreaView>
+  )
+
+  const renderSkipButton = props => <Button {...props}>Pular</Button>
+  const renderNextButton = props => <Button {...props}>Próximo</Button>
+  const renderDoneButton = props => <Button {...props}>Começar</Button>
+
   return (
     <PaperProvider theme={theme}>
-      <NavigationContainer>
-        <StatusBar backgroundColor={theme.colors.primary} />
-        <Tab.Navigator
-          initialRouteName='ProductList'
-          tabBarOptions={{
-            activeTintColor: theme.colors.primary
-          }}
-          screenOptions={({ route }) => ({
-            tabBarIcon: ({ focused, size, color }) => {
-              const iconName = getIconName(focused, route)
-              return <MCI name={iconName} size={size} color={color} />
-            }
-          })}
-        >
-          <Tab.Screen
-            name='ProductList'
-            component={ProductList}
-            options={{
-              title: 'Produtos disponíveis'
+      <StatusBar backgroundColor={theme.colors.primary} />
+      {showSlide ? (
+        <AppIntroSlider
+          data={SLIDES}
+          onDone={hideSlide}
+          onSkip={hideSlide}
+          renderItem={renderSliderItem}
+          renderSkipButton={renderSkipButton}
+          renderDoneButton={renderDoneButton}
+          renderNextButton={renderNextButton}
+          showSkipButton
+        />
+      ) : (
+        <NavigationContainer>
+          <Tab.Navigator
+            initialRouteName='ProductList'
+            tabBarOptions={{
+              activeTintColor: theme.colors.primary
             }}
-          />
-          <Tab.Screen
-            name='Catalog'
-            component={CatalogStackScreen}
-            options={{
-              title: 'Gerenciar Catálogo'
-            }}
-          />
-          <Tab.Screen
-            name='Login'
-            component={LoginScreen}
-            options={{
-              title: 'Conta'
-            }}
-          />
-        </Tab.Navigator>
-      </NavigationContainer>
+            screenOptions={({ route }) => ({
+              tabBarIcon: ({ focused, size, color }) => {
+                const iconName = getIconName(focused, route)
+                return <MCI name={iconName} size={size} color={color} />
+              }
+            })}
+          >
+            <Tab.Screen
+              name='ProductList'
+              component={ProductList}
+              options={{
+                title: 'Produtos disponíveis'
+              }}
+            />
+            <Tab.Screen
+              name='Catalog'
+              component={CatalogStackScreen}
+              options={{
+                title: 'Gerenciar Catálogo'
+              }}
+            />
+            <Tab.Screen
+              name='Login'
+              component={LoginScreen}
+              options={{
+                title: 'Conta'
+              }}
+            />
+          </Tab.Navigator>
+        </NavigationContainer>
+      )}
     </PaperProvider>
   )
 }
